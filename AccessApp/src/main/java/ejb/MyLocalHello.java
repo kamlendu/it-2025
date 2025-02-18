1ca854c68a077905979b11d0f4bbce9db7d1a722
kamlendu <kamlendup@gmail.com>
2025-02-18 12:31:41 +0530

    java

---
+++ AccessApp/src/main/java/ejb/MyLocalHello.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author root
+ */
+@Stateless
+public class MyLocalHello implements MyLocalHelloLocal {
+
+    @Override
+    public String sayLocalHello(String user) {
+       // throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    return "A Local Hello to "+ user;
+    
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+
+}
